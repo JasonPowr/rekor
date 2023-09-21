6fc8ef8f5924ddd3aebee42d6d65dd61adf9eabd
JasonPowr <20076537@mail.wit.ie>
2023-09-21 11:15:08 +0100

    test

--- cmd/backfill-redis/main.go
+++ cmd/backfill-redis/main.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+// change to trigger ci
 /*
 	backfill-redis is a script to populate the Redis index with entries
 	from Rekor. This is sometimes necessary because Redis caching is best
