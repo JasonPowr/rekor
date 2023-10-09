598cf7adc8900831ff64bd95f25b909c530f7ab1
JasonPowr <20076537@mail.wit.ie>
2023-10-09 16:52:05 +0100

    update sha

--- tests/rhtap-e2e-test/deploy-trillian_log_signer.sh
+++ tests/rhtap-e2e-test/deploy-trillian_log_signer.sh
@@ -23,5 +23,5 @@ done
 
 echo "Server is ready!"
 
-echo "Deploying trillian log server!"
-/bin/trillian_log_server --storage_system=mysql --mysql_uri="test:zaphod@tcp(127.0.0.1:3306)/test" --rpc_endpoint=0.0.0.0:8090 --http_endpoint=0.0.0.0:8091 --alsologtostderr
+echo "Deploying trillian log signer!"
+/bin/trillian_log_signer --storage_system=mysql --mysql_uri="test:zaphod@tcp(127.0.0.1:3306)/test" --rpc_endpoint=0.0.0.0:8090 --http_endpoint=0.0.0.0:8091 --force_master --alsologtostderr
