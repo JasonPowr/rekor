f0ae8db3b8c476a83df92741eb9877ff1ccce940
JasonPowr <20076537@mail.wit.ie>
2023-10-10 11:23:06 +0100

    update script

--- tests/rhtap-e2e-test/deploy-rekor_server.sh
+++ tests/rhtap-e2e-test/deploy-rekor_server.sh
@@ -24,7 +24,7 @@ done
 echo "Server is ready!"
 
 echo "Deploying the rekor server!"
-/bin/rekor-server_test -test.coverprofile=rekor-server.cov serve --trillian_log_server.address=127.0.0.1  \
+rekor-server -test.coverprofile=rekor-server.cov serve --trillian_log_server.address=127.0.0.1  \
                     --trillian_log_server.port=8090 --redis_server.address=127.0.0.1 --redis_server.port=6379 --rekor_server.address=0.0.0.0 \
                     --rekor_server.signer=memory --enable_attestation_storage   \
                     --attestation_storage_bucket=file:///var/run/attestations --max_request_body_size=32792576
