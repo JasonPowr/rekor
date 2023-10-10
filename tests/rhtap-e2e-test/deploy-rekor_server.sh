bbec30fa37d91b668d11b124a42cc56e2dcd89b9
JasonPowr <20076537@mail.wit.ie>
2023-10-10 09:02:09 +0100

    update sha

---
+++ tests/rhtap-e2e-test/deploy-rekor_server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# MySQL container details
+HOST="localhost"
+PORT="3306"
+USER="root"
+PASSWORD="zaphod"
+
+# Maximum number of attempts to ping the server
+MAX_ATTEMPTS=30
+COUNT=0
+
+# Ping the server until it's ready or the maximum number of attempts is reached
+while ! mysqladmin ping -h"$HOST" -P"$PORT" -u"$USER" -p"$PASSWORD" --silent; do
+    COUNT=$((COUNT + 1))
+    if [ $COUNT -ge $MAX_ATTEMPTS ]; then
+        echo "Server is not responding after $MAX_ATTEMPTS attempts. Exiting."
+        exit 1
+    fi
+    echo "Waiting for server to be ready..."
+    sleep 5
+done
+
+echo "Server is ready!"
+
+echo "Deploying the rekor server!"
+/bin/rekor-server -test.coverprofile=rekor-server.cov serve --trillian_log_server.address=127.0.0.1  \
+                    --trillian_log_server.port=8090 --redis_server.address=127.0.0.1 --redis_server.port=6379 --rekor_server.address=0.0.0.0 \
+                    --rekor_server.signer=memory --enable_attestation_storage   \
+                    --attestation_storage_bucket=file:///var/run/attestations --max_request_body_size=32792576
